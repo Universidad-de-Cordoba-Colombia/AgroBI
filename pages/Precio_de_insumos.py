cc5c9c3ecaa7fbac228346f85fac99995809be95
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-10-31 20:35:28 -0500

    Update Precio_de_insumos.py

--- pages/Precio_de_insumos.py
+++ pages/Precio_de_insumos.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import urllib.request
 import os
 from sqlalchemy import URL,create_engine, text
 import plotly.express as px
@@ -8,14 +9,9 @@ import pmdarima as pm
 from sympy import Point
 st.set_page_config(layout="wide")
 
-import boto3
-
-bucket_name = 'agrounicor'
-file1_key = 'datos/VistaBI.csv'
-file2_key = 'datos/vista_insumos.csv'
-s3 = boto3.client('s3', aws_access_key_id=st.secrets["AWS_ACCESS_KEY"], aws_secret_access_key=st.secrets["AWS_SECRET_KEY"])
-response = s3.get_object(Bucket=bucket_name, Key=file2_key)
-content = response['Body'].read().decode('utf-8')
+URL = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/vista_insumos.csv"
+file = urllib.request.urlopen(URL)
+content = file
 filas = content.split('\n')
 datos = []
 for fila in filas:
