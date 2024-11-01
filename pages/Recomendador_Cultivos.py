d0526cd07a05faae58b9ddb4ceb62a29c1586785
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-10-31 21:35:12 -0500

    Update Recomendador_Cultivos.py

--- pages/Recomendador_Cultivos.py
+++ pages/Recomendador_Cultivos.py
@@ -18,12 +18,12 @@ if not os.path.exists('datos'):
 
 urlinsumos = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/vista_insumos.csv"
 output_file_insumos = "datos/vista_insumos.csv"
-with urllib.request.urlopen(urlinsumos) as response1, open(output_file_insumos, 'wb2') as out_file1:
+with urllib.request.urlopen(urlinsumos) as response1, open(output_file_insumos, 'wb') as out_file1:
     shutil.copyfileobj(response, out_file1)
 
 urlBI = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/VISTABI.csv"
 output_file_BI = "datos/vista_BI.csv"
-with urllib.request.urlopen(urlBI) as response2, open(output_file_BI, 'wb2') as out_file2:
+with urllib.request.urlopen(urlBI) as response2, open(output_file_BI, 'wb') as out_file2:
     shutil.copyfileobj(response, out_file2)
 
 
