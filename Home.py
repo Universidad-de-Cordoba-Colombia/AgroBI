e96f22477e2354a615ca650d79ea80c558cf5416
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-10-31 22:52:04 -0500

    commit_31_1'_24_10_51

--- Home.py
+++ Home.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import urllib.request
+import shutil
+import os
+
+if not os.path.exists('datos'):
+    os.makedirs('datos')
+
+urlinsumos = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/vista_insumos.csv"
+output_file_insumos = "datos/vista_insumos.csv"
+with urllib.request.urlopen(urlinsumos) as response1, open(output_file_insumos, 'wb') as out_file1:
+    shutil.copyfileobj(response1, out_file1)
+
+urlBI = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/VistaBI.csv"
+output_file_BI = "datos/vista_BI.csv"
+with urllib.request.urlopen(urlBI) as response2, open(output_file_BI, 'wb') as out_file2:
+    shutil.copyfileobj(response2, out_file2)
 
 st.set_page_config(
     page_title="Hello",
--- pages/Recomendador_Cultivos.py
+++ pages/Recomendador_Cultivos.py
@@ -12,22 +12,6 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
-
-if not os.path.exists('datos'):
-    os.makedirs('datos')
-
-urlinsumos = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/vista_insumos.csv"
-output_file_insumos = "datos/vista_insumos.csv"
-with urllib.request.urlopen(urlinsumos) as response1, open(output_file_insumos, 'wb') as out_file1:
-    shutil.copyfileobj(response1, out_file1)
-
-urlBI = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/VistaBI.csv"
-output_file_BI = "datos/vista_BI.csv"
-with urllib.request.urlopen(urlBI) as response2, open(output_file_BI, 'wb') as out_file2:
-    shutil.copyfileobj(response2, out_file2)
-
-
-
 st.set_page_config(layout="wide")
 df = pd.read_csv("Crop_recommendation.csv")
 with st.sidebar:
