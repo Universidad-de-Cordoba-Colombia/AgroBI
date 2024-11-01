726d2f6998d9612fd345b12c10ad2b4723b91b6a
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-10-31 22:45:52 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,29 +1,29 @@
 import streamlit as st
-st.set_page_config(initial_sidebar_state="collapsed")
 
-col1, col2,col3 = st.columns(3)
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+initial_sidebar_state="collapsed"
+)
 
-with col1:
-  st.markdown('''
-      <a href="/Recomendador_Cultivos" target="_self">
-          <img src="https://img.freepik.com/fotos-premium/cultivo-plantas-suelo-fertil-riego-plantas-cultivo-cultivo-ideas_104677-571.jpg" height="200" />
-          <center>Recomendador de cultivo</center>
-      </a>''',
-      unsafe_allow_html=True
-  )
-with col2:
-  st.markdown('''
-      <a href="/Recomendador_Cultivos" target="_self">
-          <img src="https://images.ecestaticos.com/kZjNrDAZ0f6L5m8p91rS89Twpp0=/0x0:0x0/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fdc9%2F277%2Fac6%2Fdc9277ac614782dac6b8b32eca7417d0.jpg" height="200" />
-          <center>Recomendador de cultivo</center>
-      </a>''',
-      unsafe_allow_html=True
-  )
-with col3:
-  st.markdown('''
-      <a href="/Recomendador_Cultivos" target="_self">
-          <img src="https://images.ecestaticos.com/kZjNrDAZ0f6L5m8p91rS89Twpp0=/0x0:0x0/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fdc9%2F277%2Fac6%2Fdc9277ac614782dac6b8b32eca7417d0.jpg" height="200" />
-          <center>Recomendador de cultivo</center>
-      </a>''',
-      unsafe_allow_html=True
-  )
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
