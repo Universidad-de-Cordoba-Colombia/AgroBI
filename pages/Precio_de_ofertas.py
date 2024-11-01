5fc72c9e0183697b5995b4f4a5cba3edbeb06bc9
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-10-31 22:13:10 -0500

    Update Precio_de_ofertas.py

--- pages/Precio_de_ofertas.py
+++ pages/Precio_de_ofertas.py
@@ -9,9 +9,8 @@ import pmdarima as pm
 from sympy import Point
 st.set_page_config(layout="wide")
 
-URL = "https://agrounicor.s3.us-east-1.amazonaws.com/datos/VistaBI.csv"
-file = urllib.request.urlopen(URL)
-content = file.read().decode('utf-8')
+URL = open ('datos/Vista_BI.csv','r')
+content = URL.read()
 
 filas = content.split('\n')
 datos = []
