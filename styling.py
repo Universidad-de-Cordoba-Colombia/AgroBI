2481ae45f7bbae4291550636077f61725b52882b
samircas575 <127363877+samircas575@users.noreply.github.com>
2024-11-06 13:07:51 -0500

    Update styling.py

--- styling.py
+++ styling.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def template1_page_style():  
-    with open('..\\templates\\template1_style.css') as f:
+    with open('.\\templates\\template1_style.css') as f:
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
