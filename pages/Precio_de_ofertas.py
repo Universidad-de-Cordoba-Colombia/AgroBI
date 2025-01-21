aa2c44d76c31769fbd098ce0e9a7aeb20b10fa0e
samircas575 <127363877+samircas575@users.noreply.github.com>
2025-01-21 10:35:33 -0500

    commit_new
    ninguna

--- pages/Precio_de_ofertas.py
+++ pages/Precio_de_ofertas.py
@@ -47,7 +47,7 @@ def main_data():
     with col2:
         ff = st.date_input("Fecha Final", hoy)
     with col3:
-        option = st.selectbox('Frecuencia',('Semanal','Mensual','Quincenal'))
+        option = st.selectbox('Frecuencia-xxx',('Mensual','Quincenal','Semanal'))
     with col4:
         number = st.number_input('Predeccion %s ' %option, 1, 10, 1)
 
